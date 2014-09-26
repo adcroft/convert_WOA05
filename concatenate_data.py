3a3eb98d0a11f7b7c04716b2af1032a26a994767
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2014-09-25 22:44:26 -0400

    Added tool for joining multiple files into one file
    - I couldn't figure out how to use nco to do this when
      there is no record dimension to concatenate. It was quicker
      to write the tool than figure out how to do the conventional
      way...

---
+++ concatenate_data.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import numpy
+import netCDF4
+import argparse
+import math
+
+def parseCommandLine():
+  """
+  Parse the command line and invoke operations.
+  """
+  parser = argparse.ArgumentParser(description=
+      '''
+      Compares the data in two netcdf files and reports level-by-level stats.
+      ''',
+      epilog='Written by A.Adcroft, 2014. No support offered.')
+  parser.add_argument('inFiles', type=str,
+      metavar='FILE',
+      nargs='+',
+      help='''A netcdf file.''')
+  parser.add_argument('-o', type=str,
+      metavar='OUTFILE',
+      required=True,
+      help='''The concatenated output file.''')
+  cla = parser.parse_args()
+
+  concatenateFiles(cla.inFiles, cla.o)
+
+def concatenateFiles(inFiles, outFile):
+  """
+  Calculates level-by-level stats on 3d variables in two files.
+  """
+
+  hi = netCDF4.Dataset(inFiles[0], 'r')
+
+  # Create new file and record dimension
+  ho = netCDF4.Dataset(outFile, 'w', format='NETCDF4')
+  ho.createDimension('time',None)
+  time = ho.createVariable('time','f4')
+
+  # Copy dimensions
+  for d in hi.dimensions:
+    ho.createDimension(d, len(hi.dimensions[d])) 
+
+  # Copy global attributes
+  for a in hi.ncattrs():
+    ho.__setattr__(a ,hi.__getattr__(a))
+
+  # Create and copy variables
+  for v in hi.variables:
+    if len(hi.variables[v].shape)==1:
+      hv = ho.createVariable(v, hi.variables[v].dtype, hi.variables[v].dimensions)
+      hv[:] = hi.variables[v][:]
+    else:
+      hv = ho.createVariable(v, hi.variables[v].dtype, [u'time']+list(hi.variables[v].dimensions))
+    # Copy variable attributes
+    for a in hi.variables[v].ncattrs():
+      hv.setncattr(a, hi.variables[v].__getattr__(a))
+
+  hi.close()
+
+  # For each file, copy data
+  for n,f in zip(range(len(inFiles)),inFiles):
+    hi = netCDF4.Dataset(f, 'r')
+    for v in ho.variables:
+      if 'time' in ho.variables[v].dimensions:
+        print ho.variables[v].shape, hi.variables[v].shape,n
+        ho.variables[v][n,:] = hi.variables[v][:]
+    ho.variables['time'][n] = n
+    hi.close()
+
+  ho.close()
+
+# Invoke parseCommandLine(), the top-level prodedure
+if __name__ == '__main__': parseCommandLine()
